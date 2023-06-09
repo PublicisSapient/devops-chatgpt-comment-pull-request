6da124535b94be4749777f58137e424423172a0d
ps-aartread <Treadwell.Aaron@mayo.edu>
2023-06-09 13:40:27 -0400

    testing

--- dist/index.js
+++ dist/index.js
@@ -24985,10 +24985,14 @@ try {
       const compare_data = compareResponse.data;
       const changes = compare_data.files;
 
-      const encoded = encode(changes)
-      const tokenCount = encoded.length;
-
-      console.log(`Token count: ${tokenCount}`);
+      const str = 'This is an example sentence to try encoding out on!'
+      const encoded = encode(str)
+      console.log('Encoded this string looks like: ', encoded)
+      
+      console.log('We can look at each token and what it represents')
+      for(let token of encoded){
+        console.log({token, string: decode([token])})
+      }
 
       // console.log(changes)
       // return generate_explanation(changes);
