5b71d6adea569d2e8c54982475d2fe3f5bfa5d37
ps-aartread <Treadwell.Aaron@mayo.edu>
2023-06-19 11:07:54 -0400

    test pr title

--- index.js
+++ index.js
@@ -97,6 +97,7 @@ try {
   console.log(jsonData);
   const pullRequestNumber = jsonData.number;
   const pullRequestTitle = jsonData.title;
+  console.log('PR Title', pullRequestTitle);
   const repository = jsonData.pull_request.base.repo.full_name;
   const token = core.getInput('github-token');
 
